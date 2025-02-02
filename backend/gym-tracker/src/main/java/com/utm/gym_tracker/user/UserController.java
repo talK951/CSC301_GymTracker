b159a386b4bc2539ddd753244e3be77aa7a39b3c
andrewsmith2374 <54158695+andrewsmith2374@users.noreply.github.com>
2025-02-02 16:03:52 -0500

    Create UserController

---
+++ backend/gym-tracker/src/main/java/com/utm/gym_tracker/user/UserController.java
@@ -0,0 +1,48 @@
+package com.utm.gym_tracker.user;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
+
+@RestController
+@RequestMapping(path = "/api/user") // TODO
+public class UserController {
+    private final UserService userService;
+
+    @Autowired
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping
+    public Optional<User> getUser(
+            @RequestParam(required = false) String username,
+            @RequestParam(required = false) Long id,
+            @RequestParam(required = false) String utorID,
+            @RequestParam(required = false) String email) {
+        if (username != null) {
+            return this.userService.getUserByUsername(username);
+        } else if (id != null) {
+            return this.userService.getUserByID(id);
+        } else if (utorID != null) {
+            return this.userService.getUserByUtorID(utorID);
+        } else if (email != null) {
+            return this.userService.getUserByEmail(email);
+        }
+        return Optional.empty();
+    }
+
+    @PostMapping
+    public ResponseEntity<User> addUser(@RequestBody User user) {
+        User createdUser = this.userService.registerUser(user);
+        return new ResponseEntity<>(createdUser, HttpStatus.CREATED);
+    }
+
+//    @PutMapping
+//    public ResponseEntity<User> updateUser(@RequestParam User user) {
+//        User updatedUser = this.userService.modifyUser(user);
+//    }
+}
