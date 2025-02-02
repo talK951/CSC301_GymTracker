40e1426f7e69c5f9a6c4b51c3830768bbea2c041
andrewsmith2374 <54158695+andrewsmith2374@users.noreply.github.com>
2025-02-02 16:03:58 -0500

    Create UserRepository

---
+++ backend/gym-tracker/src/main/java/com/utm/gym_tracker/user/UserRepository.java
@@ -0,0 +1,16 @@
+package com.utm.gym_tracker.user;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, String> {
+
+    Optional<User> findByUsername(String username);
+
+    Optional<User> findByUtorID(String utorID);
+
+    Optional<User> findByEmail(String email);
+}
