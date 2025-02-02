ebf173be0023a427dbf19dea9649bc2391f69717
talk <kleimantal100@gmail.com>
2025-02-02 17:52:40 -0500

    SCRUM: 14,15,17,18 created naviagation bar which can be called and is ready for use

--- my-app/app/(auth)/_layout.tsx
+++ my-app/app/(auth)/_layout.tsx
@@ -4,6 +4,7 @@ import React from "react";
 const AuthLayout = () => {
   return (
     <Stack>
+      <Stack.Screen name="bottom-nav-bar" options={{headerShown: false }} />
       <Stack.Screen name="sign-in" options={{headerShown: false }} />
       <Stack.Screen name="sign-up" options={{headerShown: false }} />
     </Stack>
