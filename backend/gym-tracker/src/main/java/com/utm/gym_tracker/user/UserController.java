15df15d75f57caae687670ac18065d741c8638d2
Nestor_Damien <75420711+Nestor-19@users.noreply.github.com>
2025-02-03 11:32:21 -0500

    Comment out unused import

--- backend/gym-tracker/src/main/java/com/utm/gym_tracker/user/UserController.java
+++ backend/gym-tracker/src/main/java/com/utm/gym_tracker/user/UserController.java
@@ -3,7 +3,7 @@ package com.utm.gym_tracker.user;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.access.prepost.PreAuthorize;
+// import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Optional;
